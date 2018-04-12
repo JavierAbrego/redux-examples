13c9cabdd4653fa8cd69d8e703265ced16451141
Javier Abrego <jabrego@bigdata.gs>
2018-04-12 19:20:22 +0200

    Initial commit

---
+++ 2- books/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title:'title1', pages: 100},
+        {title:'title2', pages: 101},
+        {title:'title3', pages: 102},
+        {title:'title4', pages: 103}
+    ]
+}
